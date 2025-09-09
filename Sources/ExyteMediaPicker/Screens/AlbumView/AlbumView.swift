a230f1fabf642f4ac45eaac1e1a568812e9526c9
Fred Bowker <fred.bowker@locovo.co>
2025-09-09 11:26:43 +0100

    complete 2 column height camera

--- Sources/ExyteMediaPicker/Screens/AlbumView/AlbumView.swift
+++ Sources/ExyteMediaPicker/Screens/AlbumView/AlbumView.swift
@@ -2,8 +2,8 @@
 //  Created by Alex.M on 27.05.2022.
 //
 
-import SwiftUI
 import AnchoredPopup
+import SwiftUI
 
 struct AlbumView: View {
 
@@ -19,7 +19,7 @@ struct AlbumView: View {
 
     var shouldShowCamera: Bool
     var selectionParamsHolder: SelectionParamsHolder
-    var dismiss: ()->()
+    var dismiss: () -> Void
 
     @State private var fullscreenItem: AssetMediaModel.ID?
 
@@ -41,23 +41,31 @@ struct AlbumView: View {
     var content: some View {
         ScrollView {
             VStack(spacing: 0) {
-                PermissionActionView(type: .library(permissionsService.photoLibraryPermissionStatus))
+                PermissionActionView(
+                    type: .library(
+                        permissionsService.photoLibraryPermissionStatus
+                    )
+                )
 
                 if shouldShowCamera {
-                    PermissionActionView(type: .camera(permissionsService.cameraPermissionStatus))
+                    PermissionActionView(
+                        type: .camera(permissionsService.cameraPermissionStatus)
+                    )
                 }
 
                 if viewModel.isLoading, viewModel.assetMediaModels.isEmpty {
                     ProgressView()
                         .padding()
-                } else if !viewModel.isLoading, viewModel.assetMediaModels.isEmpty {
+                } else if !viewModel.isLoading,
+                    viewModel.assetMediaModels.isEmpty
+                {
                     Text("Empty data")
                         .font(.title3)
                         .foregroundColor(theme.main.pickerText)
                 } else {
                     mediasGrid
                 }
-                
+
                 Spacer()
             }
             .frame(maxWidth: .infinity)
@@ -70,15 +78,25 @@ struct AlbumView: View {
         }
     }
 
+    private var canShowLiveCameraCell: Bool {
+        #if targetEnvironment(simulator)
+            return false
+        #else
+            return shouldShowCamera
+                && permissionsService.cameraPermissionStatus == .authorized
+        #endif
+    }
+
     var mediasGrid: some View {
-        MediasGrid(viewModel.assetMediaModels) {
-#if !targetEnvironment(simulator)
-            if shouldShowCamera && permissionsService.cameraPermissionStatus == .authorized {
+        MediasGrid(
+            viewModel.assetMediaModels,
+            showingLiveCameraCell: canShowLiveCameraCell
+        ) {
+            if canShowLiveCameraCell {
                 LiveCameraCell {
                     showingCamera = true
                 }
             }
-#endif
         } content: { assetMediaModel, index, cellSize in
             cellView(assetMediaModel, index, cellSize)
         } loadingCell: {
@@ -90,50 +108,56 @@ struct AlbumView: View {
                 .aspectRatio(1, contentMode: .fit)
             }
         }
-        .onChange(of: viewModel.assetMediaModels) { _ , newValue in
+        .onChange(of: viewModel.assetMediaModels) { _, newValue in
             selectionService.updateSelection(with: newValue)
         }
     }
 
     @ViewBuilder
-    func cellView(_ assetMediaModel: AssetMediaModel, _ index: Int, _ size: CGFloat) -> some View {
+    func cellView(
+        _ assetMediaModel: AssetMediaModel,
+        _ index: Int,
+        _ size: CGFloat
+    ) -> some View {
         let imageButton = Button {
             if keyboardHeightHelper.keyboardDisplayed {
                 dismissKeyboard()
             }
-            if !selectionParamsHolder.showFullscreenPreview { // select immediately
+            if !selectionParamsHolder.showFullscreenPreview {  // select immediately
                 selectionService.onSelect(assetMediaModel: assetMediaModel)
                 if selectionService.mediaSelectionLimit == 1 {
                     dismiss()
                 }
-            }
-            else if fullscreenItem == nil {
+            } else if fullscreenItem == nil {
                 fullscreenItem = assetMediaModel.id
             }
         } label: {
             let id = "fullscreen_photo_\(index)"
-            MediaCell(viewModel: MediaViewModel(assetMediaModel: assetMediaModel), size: size)
-                .applyIf(selectionParamsHolder.showFullscreenPreview) {
-                    $0.useAsPopupAnchor(id: id) {
-                        FullscreenContainer(
-                            currentFullscreenMedia: $currentFullscreenMedia,
-                            selection: $fullscreenItem,
-                            animationID: id,
-                            assetMediaModels: viewModel.assetMediaModels,
-                            selectionParamsHolder: selectionParamsHolder,
-                            dismiss: dismiss
-                        )
-                        .environmentObject(selectionService)
-                    } customize: {
-                        $0.closeOnTap(false)
-                            .animation(.easeIn(duration: 0.2))
-                    }
-                    .simultaneousGesture(
-                        TapGesture().onEnded {
-                            fullscreenItem = assetMediaModel.id
-                        }
+            MediaCell(
+                viewModel: MediaViewModel(assetMediaModel: assetMediaModel),
+                size: size
+            )
+            .applyIf(selectionParamsHolder.showFullscreenPreview) {
+                $0.useAsPopupAnchor(id: id) {
+                    FullscreenContainer(
+                        currentFullscreenMedia: $currentFullscreenMedia,
+                        selection: $fullscreenItem,
+                        animationID: id,
+                        assetMediaModels: viewModel.assetMediaModels,
+                        selectionParamsHolder: selectionParamsHolder,
+                        dismiss: dismiss
                     )
+                    .environmentObject(selectionService)
+                } customize: {
+                    $0.closeOnTap(false)
+                        .animation(.easeIn(duration: 0.2))
                 }
+                .simultaneousGesture(
+                    TapGesture().onEnded {
+                        fullscreenItem = assetMediaModel.id
+                    }
+                )
+            }
         }
         .buttonStyle(MediaButtonStyle())
         .contentShape(Rectangle())
@@ -141,7 +165,14 @@ struct AlbumView: View {
         if selectionService.mediaSelectionLimit == 1 {
             imageButton
         } else {
-            SelectableView(selected: selectionService.index(of: assetMediaModel), isFullscreen: false, canSelect: selectionService.canSelect(assetMediaModel: assetMediaModel), selectionParamsHolder: selectionParamsHolder) {
+            SelectableView(
+                selected: selectionService.index(of: assetMediaModel),
+                isFullscreen: false,
+                canSelect: selectionService.canSelect(
+                    assetMediaModel: assetMediaModel
+                ),
+                selectionParamsHolder: selectionParamsHolder
+            ) {
                 selectionService.onSelect(assetMediaModel: assetMediaModel)
             } content: {
                 imageButton
--- Sources/ExyteMediaPicker/Utils/Widgets/MediasGrid.swift
+++ Sources/ExyteMediaPicker/Utils/Widgets/MediasGrid.swift
@@ -9,55 +9,80 @@ public struct MediasGrid<Element, Camera, Content, LoadingCell>: View
 where Element: Identifiable, Camera: View, Content: View, LoadingCell: View {
 
     public let data: [Element]
+    public let showingLiveCameraCell: Bool
     public let camera: () -> Camera
     public let content: (Element, _ index: Int, _ size: CGFloat) -> Content
     public let loadingCell: () -> LoadingCell
 
     @Environment(\.mediaPickerTheme) private var theme
 
-    public init(_ data: [Element],
-                @ViewBuilder camera: @escaping () -> Camera,
-                @ViewBuilder content: @escaping (Element, Int, CGFloat) -> Content,
-                @ViewBuilder loadingCell: @escaping () -> LoadingCell) {
+    public init(
+        _ data: [Element],
+        showingLiveCameraCell: Bool,
+        @ViewBuilder camera: @escaping () -> Camera,
+        @ViewBuilder content: @escaping (Element, Int, CGFloat) -> Content,
+        @ViewBuilder loadingCell: @escaping () -> LoadingCell
+    ) {
         self.data = data
+        self.showingLiveCameraCell = showingLiveCameraCell
         self.camera = camera
         self.content = content
         self.loadingCell = loadingCell
     }
 
     public var body: some View {
-        let (columnWidth, columns) = calculateColumnWidth(spacing: theme.cellStyle.columnsSpacing)
+        let (columnWidth, columns) = calculateColumnWidth(
+            spacing: theme.cellStyle.columnsSpacing
+        )
         let spacing = theme.cellStyle.rowSpacing
         let columnCount = 3
 
-        VStack(alignment: .leading, spacing: 0) {
-            HStack(alignment: .top, spacing: spacing) {
-                camera()
-                    .frame(width: columnWidth, height: columnWidth * 2 + spacing)
-                    .clipped()
+        if showingLiveCameraCell {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .top, spacing: spacing) {
+                    camera()
+                        .frame(
+                            width: columnWidth,
+                            height: columnWidth * 2 + spacing
+                        )
+                        .clipped()
 
-                let itemsNextToCameraCount = (columnCount - 1) * 2
-                let topData = data.prefix(itemsNextToCameraCount)
-                let topColumns = Array(repeating: GridItem(.fixed(columnWidth), spacing: spacing, alignment: .top), count: columnCount - 1)
+                    let itemsNextToCameraCount = (columnCount - 1) * 2
+                    let topData = data.prefix(itemsNextToCameraCount)
+                    let topColumns = Array(
+                        repeating: GridItem(
+                            .fixed(columnWidth),
+                            spacing: spacing,
+                            alignment: .top
+                        ),
+                        count: columnCount - 1
+                    )
 
-                LazyVGrid(columns: topColumns, spacing: spacing) {
-                    ForEach(topData.indices, id: \.self) { index in
-                        content(data[index], index, columnWidth)
+                    LazyVGrid(columns: topColumns, spacing: spacing) {
+                        ForEach(topData.indices, id: \.self) { index in
+                            content(data[index], index, columnWidth)
+                        }
                     }
                 }
+                .padding(.bottom, spacing)
+
+                let itemsInTopSection = (columnCount - 1) * 2
+                let remainingData = data.dropFirst(itemsInTopSection)
+
+                LazyVGrid(columns: columns, spacing: spacing) {
+                    ForEach(remainingData.indices, id: \.self) { index in
+                        content(data[index], index, columnWidth)
+                    }
+                    loadingCell()
+                }
             }
-            .padding(.bottom, spacing)
-
-            let itemsInTopSection = (columnCount - 1) * 2
-            let remainingData = data.dropFirst(itemsInTopSection)
-
+        } else {
             LazyVGrid(columns: columns, spacing: spacing) {
-                ForEach(remainingData.indices, id: \.self) { index in
+                ForEach(data.indices, id: \.self) { index in
                     content(data[index], index, columnWidth)
                 }
                 loadingCell()
             }
-
         }
     }
 }
